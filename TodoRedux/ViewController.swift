af2d8427c7e2e6a9032defd4dc7bf919d99eaa47
Andrew Halls <andrew@galtsoft.com>
2017-12-04 08:34:40 +0800

    Initial Commit

---
+++ TodoRedux/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoRedux
+//
+//  Created by Andrew Halls on 12/4/17.
+//  Copyright © 2017 Andrew Halls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
